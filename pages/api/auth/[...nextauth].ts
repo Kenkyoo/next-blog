c6a467752dc119545ed6fe2496929cfe643c4b9a
Franco <frfantusatti@protonmail.com>
2025-05-29 07:39:08 -0300

    install chakra and set ui components

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prisma";
+import prisma from "../../../lib/prisma";
 import { NextAuthOptions, Session } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
 
